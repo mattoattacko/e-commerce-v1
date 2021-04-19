9016d19049a40a1af9150c2612e949c3c894297a
unknown <matt.mcquain@gmail.com>
2021-04-18 19:22:40 -0700

    initial 2 item layout deployed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products, Navbar } from './components';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    //populates the products
+    setProducts(data);
+  }
+
+  // this calls our commerce products list and set the products to state
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
-      E-commerce
+      <Navbar />
+      <Products />
     </div>
   )
 }
